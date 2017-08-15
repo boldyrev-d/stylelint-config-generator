883d3e52d5920027712d9b73c5cedc85482c521a
Denis Boldyrev <boldyrev.ds@gmail.com>
2017-08-15 18:00:51 +0300

    feat: add color-no-invalid-hex rule

--- src/rules.js
+++ src/rules.js
@@ -1,30 +1,18 @@
 export default [
   {
     id: 'color-no-invalid-hex',
+    hint:
+      'Longhand hex colors can be either 6 or 8 (with alpha channel) hexadecimal characters. And their shorthand variants are 3 and 4 characters respectively.',
     variants: [
       {
         hint: 'Disallow invalid hex colors.',
-        code: 'a { color: <mark>#12345aa</mark>; }',
+        invalidCode: 'a { color: <mark>#00</mark>; }\na { color: <mark>#12345aa</mark>; }',
+        validCode: 'a { color: <mark>#000</mark>; }\na { color: <mark>#fff1a0</mark>; }',
         value: true,
       },
       {
         hint: 'Allow invalid hex colors.',
-        code: 'a { color: <mark>#00</mark>; }',
-        value: false,
-      },
-    ],
-  },
-  {
-    id: 'font-family-no-duplicate-names',
-    variants: [
-      {
-        hint: 'Disallow duplicate font family names.',
-        code: "a { font-family: 'Times', <mark>Times, serif; }",
-        value: true,
-      },
-      {
-        hint: 'Allow duplicate font family names.',
-        code: 'a { font: 1em "Arial", <mark>\'Arial\'</mark>, sans-serif; }',
+        validCode: 'a { color: <mark>#fff1az;</mark> }',
         value: false,
       },
     ],
